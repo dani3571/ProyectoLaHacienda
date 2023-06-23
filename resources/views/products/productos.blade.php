8ca6ac3a4693e53d7a8260880513b07774b17b30
Bizarr420 <jt094593@gmail.com>
2023-06-23 01:16:57 -0400

    productos

--- resources/views/products/productos.blade.php
+++ resources/views/products/productos.blade.php
@@ -23,7 +23,7 @@
             <article class="article">
                 <div class="card-body">
                     <h2 class="title">{{ Str::limit($producto->nombre, 60, '...') }}</h2>
-                    <img src="{{ asset('images/productos/' . basename($producto->image)) }}" alt="Imagen del producto"  width="50" height="50">
+                    <img src="{{ asset('images/productos/' . basename($producto->image)) }}" alt="Imagen del producto"  width="100" height="100">
                     <div class="card-body">
                         <h2 class="title">{{ Str::limit($producto->precio, 60, '...') }}</h2>
                         <p class="introduction">{{ Str::limit($producto->descripcion, 30, '...') }}</p>
